1aa4cbd3bdff26d4fcb34d6f993ff3c5c7fe8237
17Takafumi <dieu.detruit@gmail.com>
2020-04-02 23:21:20 +0900

    add color presets

--- include/graphic/color.hpp
+++ include/graphic/color.hpp
@@ -5,3 +5,21 @@
 using Color = Eigen::Vector3f;
 
 using Color4f = Eigen::Vector3f;
+
+namespace ColorPreset
+{
+
+inline const Color red{1.0f, 0.0f, 0.0f};
+inline const Color green{0.0f, 1.0f, 0.0f};
+inline const Color blue{0.0f, 0.0f, 1.0f};
+
+inline const Color yellow{1.0f, 1.0f, 0.0f};
+inline const Color skyblue{0.0f, 1.0f, 1.0f};
+inline const Color pink{1.0f, 0.0f, 1.0f};
+
+inline const Color black{0.0f, 0.0f, 0.0f};
+inline const Color white{1.0f, 1.0f, 1.0f};
+inline const Color light_gray{0.9f, 0.9f, 0.9f};
+inline const Color dark_gray{0.2f, 0.2f, 0.2f};
+
+}  // namespace ColorPreset
